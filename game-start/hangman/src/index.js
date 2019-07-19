9695a8246225b44c5eb46f16876b3e91e750a6f4
Zeus Living <zeusinthehouse@MacBook-Pro-2.local>
2019-07-19 13:41:47 -0700

    get hangman working

--- game-start/hangman/src/index.js
+++ game-start/hangman/src/index.js
@@ -4,17 +4,19 @@ $(document).ready(function() {
   //////////////////////////////
   // Setup the game
   /////////////////////////////
-  let secretsList = ["cat"]; // <------------ enter more words here
+  let secretsList = ["cat","lynx","phone","zeus","olympic"]; // <------------ enter more words here
   // Choose a secret word randomly from secretsList
   let secretWord = secretsList[Math.floor(Math.random() * secretsList.length)];
 
    // You can only guess wrong 6 times (head, body, left arm, right arm, left leg, rigth leg)
-  let maxWrongGuesses = 6;
+  let maxWrongGuesses = 3;
   let wrongGuesses = 0; // The number of times you have guessed wrong
   let correctGuesses = 0; // The number of letters you found correctly
   let guesses = []; // All the guesses you have made
 
   let $wrongGuesses = $("#wrong-guesses");
+
+  $("#maximumincorrect").html(maxWrongGuesses)
 
    for (let i = 0; i < secretWord.length; i++) {
     $("#word-spaces").append(`<div id="word-space-${i}" class="word-space"></div>`);
@@ -24,7 +26,37 @@ $(document).ready(function() {
   // Check a guess
   /////////////////////////////
   function handleGuess() {
+    const guess = $("#guess-box").val();
+    
+    console.log('im a button' + guess);
+    console.log('is ok guess result: ' + isOkGuess(guess));
+    guesses.push(guess)
+    $wrongGuesses.append(guess)
+    
+    if (guessOnWord(guess)){
+      correctGuesses=correctGuesses+1
+      console.log(correctGuesses)
+      $("#numbercorrect").html(correctGuesses)
 
+      const allIndices = getAllIndexes(secretWord, guess);
+      for (let i = 0; i < allIndices.length; i++) {
+        const j = allIndices[i];
+        $(`#word-space-${j}`).html(guess);}
+    }
+    else{
+      wrongGuesses=wrongGuesses+1
+      console.log(wrongGuesses)
+      $("#numberincorrect").html(wrongGuesses)
+
+      if (wrongGuesses==maxWrongGuesses){
+        debugger;
+      alert("GAME OVER")
+     Restart()
+     
+      
+      }
+    }
+    
   }
 
   //////////////////////////////
@@ -56,4 +88,45 @@ $(document).ready(function() {
   function isLetter(guess) {
     return allLetters.includes(guess);
   }
+  console.log('sdfd');
+  $("#im-a-button").click(function() {
+    handleGuess()
+    
+  });
+  function guessOnWord(guess){
+    const guessIsCorrect = secretWord.includes(guess);
+    console.log('answer'+ guessIsCorrect)
+    return guessIsCorrect
+  }
+
+
+  function getAllIndexes(arr, val) {
+    var indexes = [], i;
+    for(i = 0; i < arr.length; i++)
+        if (arr[i] === val)
+            indexes.push(i);
+    return indexes;
+}
+
+function Restart() {
+secretWord = secretsList[Math.floor(Math.random() * secretsList.length)];
+
+   // You can only guess wrong 6 times (head, body, left arm, right arm, left leg, rigth leg)
+  maxWrongGuesses = 6;
+  wrongGuesses = 0; // The number of times you have guessed wrong
+  correctGuesses = 0; // The number of letters you found correctly
+  guesses = []; // All the guesses you have made
+
+  $wrongGuesses = $("#wrong-guesses");
+
+  $("#maximumincorrect").html(maxWrongGuesses)
+  $("#numberincorrect").html("");
+  $("#numbercorrect").html("");
+  $("#wrong-guesses").html("");
+  $("#word-spaces").empty();
+   for (let i = 0; i < secretWord.length; i++) {
+    $("#word-spaces").append(`<div id="word-space-${i}" class="word-space"></div>`);
+  }
+}
 });
+
